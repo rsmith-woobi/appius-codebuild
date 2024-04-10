d73514230e24de5c0a1ee39123280fe58ccb0e39
rsmith-woobi <rsmith@woobi.ai>
2024-04-10 17:04:30 -0400

    removed s3.js

--- build.js
+++ build.js
@@ -6,7 +6,21 @@ import archiver from "archiver";
 
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import { syncS3Buckets } from "./s3";
+import {
+  S3Client,
+} from '@aws-sdk/client-s3';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { S3SyncClient } from 's3-sync-client';
+
+export async function syncS3Buckets(
+  source,
+  dest,
+  options,
+) {
+  const { sync } = new S3SyncClient({ client: new S3Client({}) });
+  await sync(source, dest, { del: true, ...options });
+}
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
